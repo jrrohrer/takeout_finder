3524241543d0718235da7d0f4085fe0fedcce9f6
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-11-14 19:05:49 +0000

    initial commit

---
+++ lib/takeout_finder.rb
@@ -0,0 +1,6 @@
+require "takeout_finder/version"
+
+module TakeoutFinder
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/takeout_finder/version.rb
@@ -0,0 +1,3 @@
+module TakeoutFinder
+  VERSION = "0.1.0"
+end
