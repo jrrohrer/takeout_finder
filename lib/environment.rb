e36e2f591de7abe92bb6aeea44622f1518dbaf05
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-11-16 22:10:12 +0000

    adding pry and nokogiri

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,6 +1,8 @@
 require_relative "./takeout_finder/version"
 require_relative "./takeout_finder/cli"
 
+require 'pry'
+
 module TakeoutFinder
   class Error < StandardError; end
   # Your code goes here...
