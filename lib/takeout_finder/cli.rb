866843fe35a74615435a5a6b5a3fd52e34521b8c
jrrohrer <jessie.rohrer@gmail.com>
2020-11-19 14:33:27 -0500

    refactoring CLI class

--- lib/takeout_finder/cli.rb
+++ lib/takeout_finder/cli.rb
@@ -2,12 +2,12 @@ class TakeoutFinder::CLI
   def call
     puts "\nWelcome to Takeout Finder!\n"
     puts "Please choose a restaurant category by number:"
-    restaurant_category
+    get_restaurant_category
     category_list
-    user_category_selection
+    get_user_category
   end
     
-  def restaurant_category
+  def get_restaurant_category
     #to be scraped later
     @category = ["Pizza", "Hamburgers & Hot Dogs", "Chinese", "Mexican", "Chicken Wings", "Latin American", "Brewpubs", "Sushi", "Italian", "Farmers Market", "Burgers", "Takeout", "Food Trucks"]
   end
@@ -19,9 +19,10 @@ class TakeoutFinder::CLI
     end
   end
   
-  def user_category_selection
-    category_selection = gets.strip
-    binding.pry
+  def get_user_category
+    #gets user's category selection and displays list of restaurants in chosen category
+    category_selection = gets.strip.to_i
+    display_restaurants(category_selection) if valid_input(category_selection, @category)
   end
   
   def restaurant_detail
@@ -30,5 +31,10 @@ class TakeoutFinder::CLI
   
   def valid_input(input, array)
     input.to_i <= array.length && input.to_i > 0
+  end
+
+  def display_restaurants(category_selection)
+    cuisine = @category[category_selection-1]
+    puts "Here are the local #{cuisine} restaurants:"
   end
 end
